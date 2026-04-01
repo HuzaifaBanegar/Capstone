1c0d932568b46f7fe2e7af550db7ec2245944a2c
BANEGAR HUZAIFA <banegarhuzzi1010@gmail.com>
2026-04-02 00:32:11 +0530

    feat: connected database mysql and created tables for productRepo

--- src/main/java/com/huzzi/capstone/ProductService/model/BaseModel.java
+++ src/main/java/com/huzzi/capstone/ProductService/model/BaseModel.java
@@ -1,6 +1,10 @@
 package com.huzzi.capstone.ProductService.model;
 
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +12,10 @@ import java.util.Date;
 
 @Getter
 @Setter
-
+@MappedSuperclass
 public class BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private Date created_at ;
     private Date updated_at ;
--- src/main/java/com/huzzi/capstone/ProductService/model/Category.java
+++ src/main/java/com/huzzi/capstone/ProductService/model/Category.java
@@ -1,10 +1,28 @@
 package com.huzzi.capstone.ProductService.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.util.List;
 
 @Getter
 @Setter
+@Entity
 public class Category extends BaseModel{
     private String name;
+    @OneToMany( mappedBy = "category", fetch = FetchType.EAGER) // Making fetch type Eager, by default it is Lazy
+    @JsonIgnore
+    @Fetch(FetchMode.JOIN) // Can be Select, Join , or SubSelect
+    private List<Product> products;
+
+    @Override
+    public String toString(){
+        return "{ name: "+ name +" }";
+    }
 }
